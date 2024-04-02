1cda4b157b7c4dbf960eaace54b8159a036d374c
Cokekaine <91241845+Cokekaine@users.noreply.github.com>
2024-04-02 14:46:04 +0700

    Add files via upload

---
+++ Tugas 4_PHP_0110121002_Tirta Raja_STT Terpadu Nurul Fikri/galeri.php
@@ -0,0 +1,5 @@
+<h2>Halaman Galeri</h2>
+<p>Ini adalah halaman Galeri. Berikut adalah beberapa gambar dari galeri kami:</p>
+<img src="https://cdn0-production-images-kly.akamaized.net/72FuVdD7Dq45MwMiK-QE4hGS5Lo=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2754932/original/005940800_1552970791-fotoHL_kucing.jpg" alt="Gambar 1" width="300" height="200">
+<img src="https://res.cloudinary.com/dk0z4ums3/image/upload/v1661753020/attached_image/inilah-cara-merawat-anak-kucing-yang-tepat.jpg" alt="Gambar 2" width="300" height="200">
+<img src="https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/p2/124/2024/01/13/kucing-putih-2993478675.jpg" alt="Gambar 3" width="300" height="200">
---
+++ Tugas 4_PHP_0110121002_Tirta Raja_STT Terpadu Nurul Fikri/produk.php
@@ -0,0 +1,10 @@
+<h2>Halaman Produk</h2>
+<p>Ini adalah halaman Produk. Berikut adalah daftar produk yang kami tawarkan:</p>
+<ul>
+    <li>Iphone 15</li>
+    <img src="https://cdn.eraspace.com/media/catalog/product/a/p/apple_iphone_15_blue_1_3.jpg" alt="samsung" width="300" height="200">
+    <li>Iphone 12</li>
+    <img src="https://images-cdn.ubuy.co.id/65757a651c98b276be166666-apple-iphone-12-pro-max-128gb-256gb.jpg" alt="realme" width="300" height="200">
+    <li>Iphone 11</li>
+    <img src="https://cdn.eraspace.com/media/catalog/product/a/p/apple_iphone_11_black_new_2_7_3.jpg" alt="redmi" width="300" height="200">
+</ul>
